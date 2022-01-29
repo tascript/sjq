7f0281df71346881482e8c26cef082b1e745ead6
tascript <wataruscript8611@gmail.com>
2022-01-29 21:50:01 +0900

    fix command

--- src/lib/package.ts
+++ src/lib/package.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import path from 'path'
-import { execSync, spawn } from 'child_process'
+import { spawnSync } from 'child_process'
 import chalk from 'chalk'
 import { JsonConfig } from '~/src/interface'
 import { baseName, packages } from './static'
@@ -16,10 +16,7 @@ export const install = (manager: string) => {
     case 'yarn':
       words = ['add', '-D']
   }
-  const res = spawn(manager, [...words, ...packages], { stdio: 'inherit' })
-  res.stdout?.on('daya', (c) => {
-    console.log(c.toString)
-  })
+  spawnSync(manager, [...words, ...packages], { stdio: 'inherit' })
 }
 
 export const setPrecommit = (manager: string, extension: string) => {
@@ -32,14 +29,8 @@ export const setPrecommit = (manager: string, extension: string) => {
   }
   const text = JSON.stringify(obj, null, 2)
   fs.writeFileSync(fileName, text)
-  const installRes = spawn('npx', ['husky', 'install'], { stdio: 'inherit' })
-  installRes.stdout?.on('data', (c) => {
-    console.log(c.toString)
-  })
-  const preCommitRes = spawn('npx', ['husky', 'add', '.husky/pre-commit', manager, 'lint-staged'], { stdio: 'inherit' })
-  preCommitRes.stdout?.on('data', (c) => {
-    console.log(c.toString)
-  })
+  spawnSync('npx', ['husky', 'install'], { stdio: 'inherit' })
+  spawnSync('npx', ['husky', 'add', '.husky/pre-commit', `"${manager} lint-staged"`], { stdio: 'inherit' })
 }
 
 const validJsonFile = () => {
